d065bda2a6d0be8501d7e84c9887ef76bd2ee23d
Manjil Koirala <85267300+manjilkoirala@users.noreply.github.com>
2024-02-14 22:29:38 +0545

    Completed Project 2

--- contact-form/src/Components/Button/Button.jsx
+++ contact-form/src/Components/Button/Button.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Button.css'
 const Button = ({text,icon,isOutline}) => {
+
+  
   return (
     <button className={isOutline?'outline-btn' :'primary-btn'}>
 {icon} {text}
--- contact-form/src/Components/ContactForm/ContactForm.jsx
+++ contact-form/src/Components/ContactForm/ContactForm.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ContactForm.css'
 import Button from '../Button/Button'
 import { MdCall, MdEmail, MdMessage } from 'react-icons/md'
 const ContactForm = () => {
+
+  const [name,setName]=useState()
+  const [email,setEmail]=useState()
+  const [text,setText]=useState()
+
+  const onSubmit=(e)=>{
+    e.preventDefault()
+    setName("Name: " + e.target[0].value)
+    setEmail("Email: " + e.target[1].value)
+    setText("Message: " + e.target[2].value)
+  }
+
+
+
+
   return (
     <section className='container'>
         <div className='form'>
           <div className='top-btn'>
-          <Button text="VIA SUPPORT CHAT" icon={<MdMessage fontSize="24px"/>}/>
-          <Button text='VIA CALL' icon={<MdCall fontSize="24px"/>}/>
+          <Button  text="VIA SUPPORT CHAT" icon={<MdMessage fontSize="24px"/>}/>
+          <Button  text='VIA CALL' icon={<MdCall fontSize="24px"/>}/>
     
           </div>
           <Button text='VIA EMAIL FORM' icon={<MdEmail/>} isOutline={true}></Button>
-        <form>
+        <form onSubmit={onSubmit}>
          <div className="form-control">
          <label htmlFor="name">Name</label>
           <input type="text" />
@@ -30,6 +45,8 @@ const ContactForm = () => {
          <div className="submit-btn" style={{display:'flex',justifyContent:'end'}}>
          <Button text='Submit'/>
          </div>
+
+         {name} {email} {text}
           
         </form>
         </div>
